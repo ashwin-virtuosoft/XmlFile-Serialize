dd3db04a94caff8760946da75c61a71354516e44
ashwin-virtuosoft <ashwin.kumar@virtuosoft.in>
2024-03-27 17:51:58 +0530

    Add project files.

---
+++ XmlFile/Entry.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace XmlFile
+{
+    [Serializable]
+    internal class Entry
+    {
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public int Age {  get; set; }
+        public DateTime JoiningDate {  get; set; }
+    }
+}
---
+++ XmlFile/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace XmlFile
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            SerializeObjectToXml();
+            Console.ReadLine();
+        }
+
+        private static void SerializeObjectToXml()
+        {
+            var entry = new Entry()
+            {
+                Name = "Ashwin",
+                Email = "ashwin@gmail.com",
+                Age = 22,
+                JoiningDate = DateTime.Now
+            };
+
+            var xmlSerializer=new XmlSerializer(typeof(Entry));
+            using(var writer=new StringWriter())
+            {
+                xmlSerializer.Serialize(writer, entry);
+                var xmlContent=writer.ToString();
+                Console.WriteLine(xmlContent);
+            }
+        }
+    }
+}
